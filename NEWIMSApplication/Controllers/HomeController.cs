67ebfb2d6fd1550939e843fb12d5ecdfb9a23a79
Ocsic123 <54412568+Ocsic123@users.noreply.github.com>
2019-08-25 00:11:47 +0530

    com

--- NEWIMSApplication/Controllers/HomeController.cs
+++ NEWIMSApplication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace NEWIMSApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page .";
 
             return View();
         }

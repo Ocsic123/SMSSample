bfc4e360b6c8dc5e3f2d24c7abc79eed22121c6f
Ocsic123 <54412568+Ocsic123@users.noreply.github.com>
2019-08-25 00:20:28 +0530

    code commit

---
+++ NEWIMSApplication/Controllers/UserProfilesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NEWIMSApplication.Models;
+
+namespace NEWIMSApplication.Controllers
+{
+    public class UserProfilesController : Controller
+    {
+        private SMSEntities db = new SMSEntities();
+
+        // GET: UserProfiles
+        public ActionResult Index()
+        {
+            return View(db.UserProfiles.ToList());
+        }
+
+        // GET: UserProfiles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProfile userProfile = db.UserProfiles.Find(id);
+            if (userProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserProfiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,UserName,Password,IsActive")] UserProfile userProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProfiles.Add(userProfile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProfile userProfile = db.UserProfiles.Find(id);
+            if (userProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userProfile);
+        }
+
+        // POST: UserProfiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,UserName,Password,IsActive")] UserProfile userProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userProfile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProfile userProfile = db.UserProfiles.Find(id);
+            if (userProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userProfile);
+        }
+
+        // POST: UserProfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserProfile userProfile = db.UserProfiles.Find(id);
+            db.UserProfiles.Remove(userProfile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
